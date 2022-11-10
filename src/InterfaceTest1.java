6f2c4819fb3b25005dab9120f10f7af5a1f983a1
Dhanshree03 <92053926+Dhanshree03@users.noreply.github.com>
2022-11-09 19:40:56 -0800

    Update and rename Main.java to InterfaceTest1.java

---
+++ src/InterfaceTest1.java
@@ -0,0 +1,33 @@
+
+
+
+interface pet
+{
+    public void getdata();
+}
+
+class InterfaceTest1 implements pet
+{
+    public void getdata()
+    {
+        System.out.println("Interface Method Overriden");
+    }
+
+    public static void main(String args[])
+    {
+        //pet p=new InterfaceTest1();
+        //p.getdata();
+        InterfaceTest1 i=new InterfaceTest1();
+        i.getdata();
+    }
+}
+
+
+
+
+
+
+
+
+
+
